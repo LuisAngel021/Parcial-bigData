bc2e21e5731a828f86f8b2a0450df7a35cbb5fc3
Ubuntu <ubuntu@ip-172-31-11-117.ec2.internal>
2023-03-08 12:40:29 +0000

    actualizacion

---
+++ funciones.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+import requests
+import datetime
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+client = boto3.client('s3')
+BUCKET_NAME = 'bigdata-parcial'
+
+def mi_funcion():
+    
+    url = 'https://casas.mitula.com.co/searchRE/nivel2-Bogot%C3%A1/nivel1-Cundinamarca/op-1/tipo-Casa/q-Bogot%C3%A1'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    html_file = f"{datetime.datetime.now().strftime('%Y-%m-%d')}.html"
+    client.put_object(Bucket=BUCKET_NAME,Key='yyyy-mm-dd.html',Body=response.content)
+
+mi_funcion()
