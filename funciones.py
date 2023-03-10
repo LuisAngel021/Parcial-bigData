c23698eb7f9117bb366c4075151612ef044056c6
Ubuntu <ubuntu@ip-172-31-11-117.ec2.internal>
2023-03-10 16:21:30 +0000

    Update

--- funciones.py
+++ funciones.py
@@ -8,12 +8,41 @@ from urllib.request import urlopen
 client = boto3.client('s3')
 BUCKET_NAME = 'landing-casas-021'
 
-def mi_funcion():
-    
+def capturar_html():
     url = 'https://casas.mitula.com.co/searchRE/nivel2-Bogot%C3%A1/nivel1-Cundinamarca/op-1/tipo-Casa/q-Bogot%C3%A1'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     html_file = f"{datetime.datetime.now().strftime('%Y-%m-%d')}.html"
-    client.put_object(Bucket=BUCKET_NAME,Key='yyyy-mm-dd.html',Body=response.content)
+    client.put_object(Bucket=BUCKET_NAME, Key=html_file, Body=response.content)
 
-mi_funcion()
+
+def lambda2():
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(BUCKET_NAME)
+    obj = bucket.Object(f"{datetime.datetime.now().strftime('%Y-%m-%d')}.html")
+    body = obj.get()['Body'].read()
+    soup = BeautifulSoup(body, 'html.parser')
+
+    properties = soup.find_all('div', {'class': 'listing-card__information-main'})
+
+    data = []
+
+    for property in properties:
+        price = property.find('div', {'class': 'listing-card__price-wrapper'}).text.strip()
+        sqft = property.find_all('div', {'class': 'listing-card__property'})
+        sqft2 = sqft    
+        casa = [price]
+        for property2 in sqft2:
+            casa.append(property2.text.strip())
+          
+        data.append(casa)    
+    
+    print(data)
+    s = ""
+    s = s + " Precio, num_habitaciones, num_banos, metros_cuadrados\n"
+    for fila in data:
+        if len(fila) >= 4:
+            s = s + fila[0] + "," + fila[1].replace(".", " ") + "," + fila[2].replace(".", " ") + "," + fila[3] +"\n"
+        
+    client = boto3.client('s3')
+    client.put_object(Body=s, Bucket='casas-final-021', Key=f"{datetime.datetime.now().strftime('%Y-%m-%d')}.csv")
--- test_funciones.py
+++ test_funciones.py
@@ -2,13 +2,15 @@ import json
 import boto3
 import requests
 import datetime
+from unittest import mock
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from funciones import mi_funcion
+from funciones import capturar_html, lambda2
+from unittest.mock import MagicMock, patch
 
-def test_mi_funcion():
+def test_capturar_html():
 
-    mi_funcion()
+    capturar_html()
     
     s3 = boto3.resource('s3')
     bucket = s3.Bucket('landing-casas-021')
@@ -16,3 +18,42 @@ def test_mi_funcion():
     assert len(objs) == 1
     
     
+@patch('boto3.resource')
+@patch('boto3.client')
+def test_lambda2(mock_s3_client, mock_s3_resource):
+    # Simula el objeto S3 y los métodos get() y put_object()
+    mock_bucket = MagicMock()
+    mock_object = MagicMock()
+    mock_object.get.return_value = {'Body': MagicMock(read=MagicMock(return_value=b'<html><body><div class="listing-card__information-main"><div class="listing-card__price-wrapper">Price</div><div class="listing-card__property">1</div><div class="listing-card__property">2</div><div class="listing-card__property">3</div></div></body></html>'))}
+    mock_bucket.Object.return_value = mock_object
+    mock_s3_resource.return_value.Bucket.return_value = mock_bucket
+    mock_s3_client.return_value.put_object.return_value = {}
+
+    # Ejecuta la función lambda2()
+    lambda2()
+
+    # Comprueba que el método put_object() fue llamado con los parámetros correctos
+    mock_s3_client.return_value.put_object.assert_called_with(
+        Body=' Precio, num_habitaciones, num_banos, metros_cuadrados\nPrice,1,2,3\n', 
+        Bucket='casas-final-021', 
+        Key=f"{datetime.datetime.now().strftime('%Y-%m-%d')}.csv"
+    )
+
+def test_capturar_html_con_mock():
+    
+    mock_requests = MagicMock()
+    mock_response = MagicMock()
+    mock_response.content = b'contenido.html'
+    mock_requests.get.return_value = mock_response
+
+    
+    mock_boto3 = MagicMock()
+    mock_client = MagicMock()
+    mock_boto3.client.return_value = mock_client
+
+    
+    with patch.dict('sys.modules', {'boto3': mock_boto3, 'requests': mock_requests}):
+        capturar_html()
+
+    
+    mock_client.put_object(Bucket='landing-casas-021', Key=mock.ANY, Body=mock_response.content)
